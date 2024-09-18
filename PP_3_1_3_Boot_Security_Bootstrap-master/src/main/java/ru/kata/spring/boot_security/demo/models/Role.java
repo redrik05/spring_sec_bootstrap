294caed5c4e38ede643feff5dd4b7119bbd75e0a
Redrik <79138016+redrik05@users.noreply.github.com>
2024-09-18 03:16:40 +0300

    Update Role.java

--- PP_3_1_3_Boot_Security_Bootstrap-master/src/main/java/ru/kata/spring/boot_security/demo/models/Role.java
+++ PP_3_1_3_Boot_Security_Bootstrap-master/src/main/java/ru/kata/spring/boot_security/demo/models/Role.java
@@ -12,10 +12,8 @@ public class Role implements GrantedAuthority {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
     private Long id;
 
-    @Column(name = "name")
     private String name;
 
     @ManyToMany(mappedBy = "roles", fetch = FetchType.LAZY)
